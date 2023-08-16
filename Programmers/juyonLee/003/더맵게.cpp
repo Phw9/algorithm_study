a9970054d5a6296286da32d3dd637f2f0035b642
juyonLee00 <ljy012900@gmail.com>
2023-08-16 13:55:59 +0900

    가장큰수

--- Programmers/juyonLee/003/더맵게.cpp
+++ Programmers/juyonLee/003/더맵게.cpp
@@ -26,3 +26,60 @@ int solution(vector<int> scoville, int K) {
     
     return answer;
 }
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    public Text timeTxt;
+    float time;
+    public GameObject card;
+    public GameObject retryBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i =0; i<16; i++)  
+        {
+            GameObject newCard = Instantiate(card);
+            newCard.transform.parent = GameObject.Find("cards").transform;
+
+            float x = (i / 4) * 1.4f - 2.1f;
+            float y = (i % 4) * 1.4f - 3.0f;
+            newCard.transform.position = new Vector3(x, y, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+        */
+        if(time < 2.0f)
+        {
+            time += Time.deltaTime;
+            timeTxt.text = time.ToString("N2");
+
+            //Time.timeScale = 0.0f;
+            //retryBtn.SetActive(true);
+        }
+
+        else
+        {
+            retryBtn.SetActive(true);
+        }
+        /*
+        if(retryBtn.activeSelf == false)
+        {
+            Time.timeScale = 1.0f;
+        }
+        */
+    }
+}
+
